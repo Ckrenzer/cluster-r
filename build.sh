713c9b5290411461af7bf8d52dd403423fa63656
Ckrenzer <ckrenzer.info@gmail.com>
2025-03-17 13:49:16 -0400

    pushing image to registry

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/bash
 git_hash="$(git rev-parse --short HEAD)"
-registry_name="ckrenzer/cluster-r"
+username="ckrenzer"
+registry_name="${username}/cluster-r"
+registry_password="$(awk '{print $2}' ~/.dockerlogin)"
 docker build --build-arg GITHASH="${git_hash}" --tag "${registry_name}":data.table .
+echo "${registry_password}" | docker login -u "${username}" --password-stdin
 docker push "${registry_name}":data.table
+docker logout
