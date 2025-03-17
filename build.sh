341f45e2922fea219058560e90e64b6e398a4cea
Ckrenzer <ckrenzer.info@gmail.com>
2025-03-17 12:54:03 -0400

    adding label with git hash, pushing to dockerhub

--- build.sh
+++ build.sh
@@ -1,2 +1,5 @@
+#!/usr/bin/bash
 git_hash="$(git rev-parse --short HEAD)"
-docker build --build-arg GITHASH="$git_hash" --tag ckrenzer/cluster-r:data.table .
+registry_name="ckrenzer/cluster-r"
+docker build --build-arg GITHASH="${git_hash}" --tag "${registry_name}":data.table .
+docker push "${registry_name}":data.table
